16bb64194e5c8d7563e2c98c433b4002b3ff46ef
Sano Suguru <sano@mbp17.local>
2018-10-08 17:54:47 +0900

    Learned about basics of xcode

--- SwiftButton1/NextViewController.swift
+++ SwiftButton1/NextViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  NextViewController.swift
 //  SwiftButton1
 //
 //  Created by Sano Suguru on 2018/10/08.
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    var count = 5
+class NextViewController: UIViewController {
+    @IBOutlet var changeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        count = 10
-        print(count)
     }
     
-    @IBAction func tap(_ sender: Any) {
-        
+    @IBAction func change(_ sender: Any) {
+        changeLabel.text = "暗号が解除されました。"
     }
 }
-
