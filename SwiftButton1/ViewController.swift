d72a5685072fe0d2e29d4b73ca51d2f533d16b9f
Sano Suguru <sano@mbp17.local>
2018-10-08 16:47:51 +0900

    Initial Commit

---
+++ SwiftButton1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftButton1
+//
+//  Created by Sano Suguru on 2018/10/08.
+//  Copyright © 2018年 Sano Suguru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
